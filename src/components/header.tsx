29aab911a5d827455bcd719581d0c81f120ba8cc
parksanghyun <basira@naver.com>
2026-02-11 09:06:54 +0900

    mobile 완료

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,15 +5,8 @@ export default function Header() {
   return (
     <header>
       <div className="inner">
-        <h1>
-          <Link href="/">
-            <Image
-              src="/assets/images/logo.svg"
-              alt="로고"
-              width={150}
-              height={40}
-            />
-          </Link>
+        <h1 className="logo">
+          <Link href="/"></Link>
         </h1>
       </div>
     </header>
